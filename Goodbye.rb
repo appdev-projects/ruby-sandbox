ad7325e3cb7b3a839ebe6fc736999a9f294f9b97
Bella Rose Wilson <bwilson9893@gmail.com>
2025-01-17 21:32:38 +0000

    yay it worked for someone else but not 4 me save

---
+++ Goodbye.rb
@@ -0,0 +1,9 @@
+#/goodbye.rb 
+pp "See ya later!"
+
+#/howdy.rb
+my_string = "Hello,world!"
+pp my_string
+
+require "./goodbye.rb"
+
--- howdy.rb
+++ howdy.rb
@@ -1,2 +1,5 @@
 #testing 
-my_string = "Hello, world"
+pp "What's your name?"
+their_name = gets 
+
+pp "Hello," + their_name + "!" 
